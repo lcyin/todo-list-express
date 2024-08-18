4db3caa3ba472ffec89f4e077dfad28395708e79
lcyin <laichunyinkevin@gmailcom>
2024-08-18 18:25:51 +0800

    Update todo migration file

--- src/db/migrations/00000002_Init_todo.sql
+++ src/db/migrations/00000002_Init_todo.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS todos (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   title TEXT,
   description TEXT,
-  completed boolean,
+  completed boolean DEFAULT false,
   created_at TIMESTAMP DEFAULT NOW()
 );
